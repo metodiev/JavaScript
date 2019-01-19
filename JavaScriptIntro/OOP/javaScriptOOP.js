9222db93461cf0a5433773629cf900bbcfaef914
metodiev <miroslav.l.metodiev@gmail.com>
2019-01-19 20:19:33 +0200

    OOP stuff

---
+++ JavaScriptIntro/OOP/javaScriptOOP.js
@@ -0,0 +1,33 @@
+
+
+<!DOCTYPE html>
+<html>
+<title>Web Page Design</title>
+<head>
+<script>
+class OOP{
+
+constructor () {
+    
+}
+
+
+ Sum( a,  b) {
+        return a +b;
+    }
+
+    nameFunc(){
+      return  this.name;
+    }
+
+}
+
+let oopObjectClass  = new OOP();
+oopObjectClass.nameFunc();
+
+
+</script>
+</head>
+<body>
+</body>
+</html>
